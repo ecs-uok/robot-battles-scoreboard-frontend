39e3331c3d9b31ac42eb3777d27b12ca93e374b8
Binath Perera <binathperera@bluelotus360.co>
2024-06-04 21:29:02 +0530

    navigation changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,17 @@
 // import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar.tsx";
 
 import FirstPage from "./pages/firstPage.tsx";
-import SecondPage from "./pages/secondPage.tsx";
-import ThirdPage from "./pages/thirdPage.tsx";
-import FourthPage from "./pages/fourthPage.tsx";
-import FifthPage from "./pages/fifthPage.tsx";
+import AdminPage from "./pages/adminPage.tsx";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
         <Routes>
           <Route path="/" element={<FirstPage />} />
-          <Route path="/second" element={<SecondPage />} />
-          <Route path="/third" element={<ThirdPage />} />
-          <Route path="/fourth" element={<FourthPage />} />
-          <Route path="/fifth" element={<FifthPage />} />
+          <Route path="/admin/*" element={<AdminPage />} />
         </Routes>
       </BrowserRouter>
     </>
