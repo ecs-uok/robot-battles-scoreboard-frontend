9112e3e6f2300acb4cac2d4763e44f16490b72eb
Binath Perera <binathperera@bluelotus360.co>
2024-06-04 20:45:30 +0530

    changed app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // import logo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [mainTime, setMainTime] = useState();
+  const [pitTime, setPitTime] = useState();
+  useEffect(() => {
+    const eventSource = new EventSource(
+      "https://robot-battles-scoreboard-backend.onrender.com/timer"
+    );
+    if (typeof eventSource != undefined) {
+      console.log("Connection with timer successful");
+      eventSource.onmessage = (event) => {
+        const eventData = JSON.parse(event.data);
+        console.log(eventData);
+        setMainTime(eventData.mainTime);
+        setPitTime(eventData.pitTime);
+      };
+    } else {
+      console.log("Coudn't connect to timer");
+    }
+    return () => eventSource.close();
+  }, []);
 
   return (
     <>
       <div>{/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}</div>
       <h1>Scoreboard</h1>
-      <div className="card"></div>
+      <div className="card">
+        <h1>{mainTime}</h1>
+        <h3>{pitTime}</h3>
+      </div>
       <p className="read-the-docs">Copyrights ECSC 2024</p>
     </>
   );
