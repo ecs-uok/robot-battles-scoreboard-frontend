96340c60823ea969af77d6336f8289a9da2e9f5e
Binath Perera <binathperera@bluelotus360.co>
2024-06-05 22:45:28 +0530

    Stable commit

--- src/components/Creategame/Creategame.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Creategame() {
-  return (
-    <div>Creategame</div>
-  )
-}
-
-export default Creategame
---
+++ src/pages/adminPage.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "../components/Navbar.tsx";
+import SecondPage from "./secondPage.tsx";
+import ThirdPage from "./thirdPage.tsx";
+import FourthPage from "./fourthPage.tsx";
+import FifthPage from "./fifthPage.tsx";
+function adminPage() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<SecondPage />} />
+        <Route path="/third" element={<ThirdPage />} />
+        <Route path="/fourth" element={<FourthPage />} />
+        <Route path="/fifth" element={<FifthPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default adminPage;
