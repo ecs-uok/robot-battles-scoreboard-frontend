cc6d2e1c8f95bb543dd5c48e65d6bd87b774848d
Binath Perera <binathperera@bluelotus360.co>
2024-06-07 21:59:09 +0530

    changes to create game page

--- src/pages/secondPage.tsx
+++ src/pages/secondPage.tsx
@@ -29,9 +29,73 @@ async function getTeamList() {
     select2 ? select2.appendChild(el2) : select2;
   }
 }
+
 function SecondPage() {
   getTeamList();
+  const [gameNo, setGameNo] = useState(1);
+  const [team1id, setTeam1id] = useState("0");
+  const [team2id, setTeam2id] = useState("0");
+  var totalTime = 180;
+  var pitOpenTime = 60;
+  var pitTime = 20;
+  fetch("http://localhost:5000/nextGameId")
+    .then((response) => response.json())
+    .then((json) => {
+      setGameNo(json.gameId);
+    });
+  function setGameDetails() {
+    var gameNo = document.getElementById("gameNo")?.innerText;
+    var body = JSON.stringify({
+      gameId: `${gameNo}`,
+      team1: team1id,
+      team2: team2id,
+    });
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: body,
+    };
+    fetch(
+      "https://robot-battles-scoreboard-backend.onrender.com/setGameDetails",
+      requestOptions
+    );
 
+    const requestOptions2 = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        mainTime: totalTime,
+      }),
+    };
+    fetch(
+      "https://robot-battles-scoreboard-backend.onrender.com/setMain",
+      requestOptions2
+    );
+
+    const requestOptions3 = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        pitTime: pitTime,
+      }),
+    };
+    fetch(
+      "https://robot-battles-scoreboard-backend.onrender.com/setPit",
+      requestOptions3
+    );
+
+    const requestOptions4 = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        pitOpenTime: pitOpenTime,
+      }),
+    };
+    fetch(
+      "https://robot-battles-scoreboard-backend.onrender.com/setPitOpen",
+      requestOptions4
+    );
+  }
   return (
     <div
       className="font-custom"
@@ -62,6 +126,7 @@ function SecondPage() {
           <select
             id="teamA"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            onChange={(e) => setTeam1id(e.target.value)}
           ></select>
           {/* <input
             type="text"
@@ -85,6 +150,7 @@ function SecondPage() {
             <select
               id="teamB"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              onChange={(e) => setTeam2id(e.target.value)}
             ></select>
             {/* <input
               type="text"
@@ -100,9 +166,23 @@ function SecondPage() {
         </div>
       </div>
 
-      <div className="text-center text-white text-2xl mt-10">Game No: 00</div>
+      <div className="text-center text-white text-2xl mt-10">
+        Total time: {totalTime} s
+      </div>
+      <div className="text-center text-white text-2xl mt-10">
+        Pit opens at: {pitOpenTime} s
+      </div>
+      <div className="text-center text-white text-2xl mt-10">
+        Pit time: {pitTime} s
+      </div>
+      <div className="text-center text-white text-2xl mt-10">
+        Game No <span id="gameNo">{gameNo}</span>
+      </div>
       <div className="flex flex-row justify-center my-5">
-        <button className="bg-yellow-300 hover:bg-yellow-500 text-black font-bold py-2 px-4 rounded-lg  ">
+        <button
+          className="bg-yellow-300 hover:bg-yellow-500 text-black font-bold py-2 px-4 rounded-lg  "
+          onClick={setGameDetails}
+        >
           CREATE GAME
         </button>
       </div>
