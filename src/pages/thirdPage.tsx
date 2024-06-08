25cc71fb2af2345eea3d83e13c75584ee65c38e0
Binath Perera <binathperera@bluelotus360.co>
2024-06-08 09:34:37 +0530

    changes

--- src/pages/thirdPage.tsx
+++ src/pages/thirdPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "../styles/scoreboard.css";
-
+import AOS from "aos";
 //images
 import TitleImg from "../assets/Images/scoreboard-title.png";
 import bgImg from "../assets/Images/scoreboard-background.png";
@@ -9,19 +9,70 @@ import sampleLogo1 from "../assets/Images/sample-team-logo-1.png";
 import sampleLogo2 from "../assets/Images/sample-team-logo-2.png";
 
 function thirdPage() {
-  // const [gameNo, setGameNo] = useState();
+  const [gameNo, setGameNo] = useState();
+  var team1Id: number;
+  var team2Id: number;
 
   const [mainTime, setMainTime] = useState();
   const [pitTime, setPitTime] = useState();
 
-  // const [team1name, setTeam1Name] = useState();
-  // const [team2name, setTeam2Name] = useState();
+  const [team1name, setTeam1Name] = useState();
+  const [team2name, setTeam2Name] = useState();
 
-  // const [team1Leader, setTeam1Leader] = useState();
-  // const [team2Leader, setTeam2Leader] = useState();
+  const [team1Leader, setTeam1Leader] = useState();
+  const [team2Leader, setTeam2Leader] = useState();
 
-  // const [team1Logo, setTeam1Logo] = useState();
-  // const [team2Logo, setTeam2Logo] = useState();
+  const [team1Logo, setTeam1Logo] = useState();
+  const [team2Logo, setTeam2Logo] = useState();
+
+  async function setTeamInfo() {
+    fetch(
+      "https://robot-battles-scoreboard-backend.onrender.com/getGameDetails"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setTeam1Name(data.team1.name);
+        setTeam2Name(data.team2.name);
+        setTeam1Logo(data.team1.logo);
+        setTeam2Logo(data.team2.logo);
+        setTeam1Leader(data.team1.leader);
+        setTeam2Leader(data.team2.leader);
+      });
+  }
+
+  useEffect(() => {
+    AOS.init();
+    const eventSource = new EventSource(
+      "https://robot-battles-scoreboard-backend.onrender.com/timer"
+    );
+    if (typeof eventSource != undefined) {
+      console.log("Connection with timer successful");
+      let oldVal = -1;
+      eventSource.onmessage = (event) => {
+        const eventData = JSON.parse(event.data);
+        console.log(eventData);
+        setMainTime(eventData.mainTime);
+        setPitTime(eventData.pitTime);
+        setGameNo(eventData.gameId);
+
+        if (
+          oldVal != eventData.gameId ||
+          team1Id != eventData.team1Id ||
+          team2Id != eventData.team2Id
+        ) {
+          oldVal = eventData.gameId;
+          team1Id = eventData.team1Id;
+          team2Id = eventData.team2Id;
+          console.log("Game details changed");
+          setTeamInfo();
+        }
+      };
+    } else {
+      console.log("Coudn't connect to timer");
+    }
+    return () => eventSource.close();
+  }, []);
 
   return (
     <div
@@ -45,18 +96,25 @@ function thirdPage() {
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-12 pt-5 mx-8  ">
         <div className="md:col-span-3 lg:col-span-3  ">
           <img
-            src={sampleLogo1}
+            src={team1Logo}
             className="w-4/5 mx-auto"
             style={{ paddingRight: "10px", backgroundColor: "#0DECC4" }}
           />
           <div className="text-center text-white text-xl">TEAM 1</div>
-          <div className="text-center text-yellow-300 text-2xl">ROBOTS</div>
+          <div className="text-center text-yellow-300 text-2xl">
+            {team1name}
+          </div>
         </div>
         <div className="md:col-span-3 lg:col-span-6 ">
           <div className="text-3xl text-center text-white">TIME REMAINING</div>
           <div className="text-8xl text-center text-white">
-            {mainTime ? Math.floor(mainTime / 60) : mainTime || "00"}
-            {mainTime ? mainTime % 60 : mainTime || "00"}
+            {mainTime ? Math.floor(mainTime / 60) : mainTime || "00"}:
+            {mainTime
+              ? (mainTime % 60).toLocaleString("en-US", {
+                  minimumIntegerDigits: 2,
+                  useGrouping: false,
+                })
+              : mainTime || "00"}
           </div>
           <hr className="border-2 border-white my-5" />
           <div className="text-2xl text-center text-white">ADDITIONAL TIME</div>
@@ -68,12 +126,14 @@ function thirdPage() {
 
         <div className="md:col-span-3 lg:col-span-3  ">
           <img
-            src={sampleLogo2}
+            src={team2Logo}
             className="w-4/5 mx-auto"
             style={{ paddingLeft: "10px", backgroundColor: "#001AFF" }}
           />
           <div className="text-center text-white text-xl">TEAM 1</div>
-          <div className="text-center text-yellow-300 text-2xl">MACHINE</div>
+          <div className="text-center text-yellow-300 text-2xl">
+            {team2name}
+          </div>
         </div>
       </div>
 
