01fa4324b3cbdf0dbb8041cf8bc8126384d600f2
Rusiru Randika <rusirurandikakmv@gmail.com>
2025-09-20 13:38:53 +0530

    Update iframe source in Draw component for improved functionality and styling

--- src/pages/Draw.tsx
+++ src/pages/Draw.tsx
@@ -1,12 +1,11 @@
 const Draw = () => {
     return (
-        <>
-            <iframe
-                src="https://qrkit.co/1UEbXw"
-                className="h-screen w-screen"
-            >
-            </iframe>
-        </>
+        <iframe 
+            style={{border: "1px solid rgba(0, 0, 0, 0.1)"}} 
+            src="https://embed.figma.com/proto/Dj8hGeBTG1Q3GlwBWXgh7g/Untitled?node-id=134-4502&scaling=min-zoom&content-scaling=fixed&page-id=110%3A98&embed-host=share" 
+            allowFullScreen
+            className="w-screen h-screen"
+        />
     )
 }
 
